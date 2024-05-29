4d1b5395626e3595cdbb26d57e51038a2ce923c6
Domi_Nate <jdomi@gmail.com>
2024-05-29 19:52:58 +0200

    Végkifejlet

---
+++ Juhasz_Dominik_Pentashop/utilities/jsonReader.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pentashop_Teszt_Juhasz_Dominik.utilities
+{
+    public class jsonReader
+    {
+        public List<string> productsDataList(string tokenName)
+        {
+            string myJsonString = File.ReadAllText("tests/usersProfile.json");
+            var jsonObject = JToken.Parse(myJsonString);
+            return jsonObject.SelectTokens(tokenName).Values<string>().ToList();
+        }
+    }
+}
